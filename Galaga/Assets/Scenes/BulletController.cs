c3d1b89c8119c8456b96a9d7756dbef24cad15c2
SJY <jun000405@naver.com>
2023-08-23 00:59:55 +0900

    제발 됐으면

---
+++ Galaga/Assets/Scenes/BulletController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    public float speed = 1.0f;
+
+    private void Start()
+    {
+        this.rigid2D = gameObject.GetComponent<Rigidbody2D>();
+        // AddForce
+        // this.rigid2D.AddForce(transform.up * speed);
+    }
+    // Update is called once per fram
+    void Update()
+    {
+        // tranform ÀÌµ¿
+        transform.Translate(0, speed * Time.deltaTime, 0);
+
+        if (transform.position.x > 9.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
